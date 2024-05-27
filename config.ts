76285b21f7c4e7c6133ea4942c3afd17ea103d18
David Carvajal <david@tropykus.com>
2024-05-27 10:54:14 -0500

    Added uniswap functions

---
+++ config.ts
@@ -0,0 +1,29 @@
+import { Token } from "@uniswap/sdk-core";
+import { FeeAmount } from "@uniswap/v3-sdk";
+import { USDC_TOKEN, COPM_TOKEN } from "./constants";
+
+export interface BaseConfig {
+	rpc: {
+		local: string;
+		mainnet: string;
+	};
+	tokens: {
+		in: Token;
+		amountIn: number;
+		out: Token;
+		poolFee: number;
+	};
+}
+
+export const CurrentConfig: BaseConfig = {
+	rpc: {
+		local: "http://localhost:8545",
+		mainnet: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+	},
+	tokens: {
+		in: COPM_TOKEN,
+		amountIn: 10000,
+		out: USDC_TOKEN,
+		poolFee: FeeAmount.MEDIUM,
+	},
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,25 +5,15 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
   polygon,
-  sepolia,
 } from 'wagmi/chains';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
-const config = getDefaultConfig({
+export const wagmiConfig = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
-    mainnet,
     polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
   ssr: true,
 });
@@ -32,7 +22,7 @@ const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={client}>
         <RainbowKitProvider>
           <Component {...pageProps} />
